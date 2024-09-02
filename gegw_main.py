c46accb6b0958c9eafc2ce9a488673da26087034
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2024-09-03 00:54:50 +0900

    関ヶ原のページの大枠が完成

---
+++ gegw_main.py
@@ -0,0 +1,55 @@
+import sys
+
+from ruby import ruby
+from template_to_pages import template_to_pages
+
+def main(only_ruby=False, only_page=False):
+    if not only_page:
+        DATA_FILE_PATH = "gegw_data/data.csv"
+        RESULT_FILE_PATH = "gegw_data/data_with_ruby.csv"
+        SELF_RUBY_DATA_FILE_PATH = "gegw_data/ruby_data.csv"
+
+
+        DONT_NEED_RUBY_COLUMNS = [
+            "教材順", "カード順", "イラスト説明1キャラ", "イラスト説明2キャラ", "どうしてQキャラ", 
+            "関ヶ原ではQキャラ", "関ヶ原の後Qキャラ", "カード画像", 
+            "パラメータ1（兵数）", "パラメータ2（知略）", "パラメータ3（武勇）", "パラメータ4（統率）", "パラメータ5の名前", "パラメータ5の値",
+            "家紋画像"
+        ]
+
+        ruby(DATA_FILE_PATH, RESULT_FILE_PATH, SELF_RUBY_DATA_FILE_PATH, DONT_NEED_RUBY_COLUMNS)
+
+    if only_ruby:
+        return
+
+
+    FIELDS = [
+        "異名", "名前", "時代", "身分", "生没年",
+        "イラスト説明1キャラ", "イラスト説明1のタイトル", "イラスト説明1",
+        "イラスト説明2キャラ", "イラスト説明2のタイトル", "イラスト説明2", 
+        "効果名1", "効果名2", "どうしてQ", "どうしてQキャラ", "どうしてA",
+        "関ヶ原ではQ", "関ヶ原ではQキャラ", "関ヶ原ではA", "関ヶ原の後Q",
+        "関ヶ原の後Qキャラ", "関ヶ原の後A", "クイズ", "コメント", "カード画像", "東西",
+        "パラメータ1（兵数）", "パラメータ2（知略）", "パラメータ3（武勇）", "パラメータ4（統率）",
+        "パラメータ5の名前", "パラメータ5の値", "家紋説明", "家紋画像", "家紋名"
+    ]
+
+    DATA_WITH_RUBY_FILE_PATH = "gegw_data/data_with_ruby.csv"
+    TEMPLATE_FILE_PATH = "gegw_templates/template.html"
+    INDEX_TEMPLATE_FILE_PATH = "gegw_templates/index_template.html"
+    PAGES_DIV_TEMPLATE_FILE_PATH = "gegw_templates/pages_div_template.html"
+    ALL_TEMPLATE_FILE_PATH = "gegw_templates/all_template.html"
+    RESULT_DIR = "gegw_pages/"
+
+    template_to_pages(
+        FIELDS, DATA_WITH_RUBY_FILE_PATH, TEMPLATE_FILE_PATH, 
+        INDEX_TEMPLATE_FILE_PATH, PAGES_DIV_TEMPLATE_FILE_PATH, 
+        ALL_TEMPLATE_FILE_PATH, RESULT_DIR)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        main()
+    elif sys.argv[1] == "ruby":
+        main(only_ruby=True)
+    elif sys.argv[1] == "page":
+        main(only_page=True)
--- template_to_pages.py
+++ template_to_pages.py
@@ -2,104 +2,119 @@ import os
 import csv
 import re
 
-fields = [
-    "異名",
-    "名前",
-    "時代",
-    "身分",
-    "生没年",
-    "効果解説トップキャラ",
-    "効果名1",
-    "効果解説1",
-    "効果解説1キャラ",
-    "効果名2",
-    "効果解説2",
-    "効果解説2キャラ",
-    "コラム",
-    "どんな人？",
-    "どんな人？トップキャラ",
-    "どんな人？内容キャラ",
-    "豆知識",
-    # "豆知識キャラ",
-    "クイズ",
-    "イラスト説明1のタイトル",
-    "イラスト説明1",
-    "イラスト説明1キャラ",
-    "イラスト説明2のタイトル",
-    "イラスト説明2",
-    "イラスト説明2キャラ",
-    "カード画像"
-]
 
-DATA_WITH_RUBY_FILE_PATH = "data_with_ruby.csv"
-TEMPLATE_FILE_PATH = "templates/template.html"
-INDEX_TEMPLATE_FILE_PATH = "templates/index_template.html"
-PAGES_DIV_TEMPLATE_FILE_PATH = "templates/pages_div_template.html"
-RESULT_DIR = "pages/"
+def template_to_pages(
+    FIELDS, DATA_WITH_RUBY_FILE_PATH, TEMPLATE_FILE_PATH,
+    INDEX_TEMPLATE_FILE_PATH, PAGES_DIV_TEMPLATE_FILE_PATH, 
+    ALL_TEMPLATE_FILE_PATH, RESULT_DIR
+):
 
+    def remove_ruby_tags(text):
+        """<rt>タグの中身と<ruby>タグを削除する。（<ruby>の中身の漢字は残す）"""
+        return re.sub(r'<rt>.*?</rt>|<ruby>|</ruby>', '', text)
 
-def remove_ruby_tags(text):
-    """<rt>タグの中身と<ruby>タグを削除する。（<ruby>の中身の漢字は残す）"""
-    return re.sub(r'<rt>.*?</rt>|<ruby>|</ruby>', '', text)
-
-def create_index_page(names_without_ruby):
-    # pages/index.html を生成（各ページへのリンクをひたすら並べる）
-    with open(INDEX_TEMPLATE_FILE_PATH, "r", encoding="utf-8") as template_f:
-        template = template_f.read()
-        links = ""
-        for name in names_without_ruby:
-            links += f'<li><a href="{name}/index.html">{name}</a></li>\n'
-        template = template.replace("{links}", links)
-        with open(os.path.join(RESULT_DIR, "index.html"), "w", encoding="utf-8") as result_f:
-            result_f.write(template)
-            
-def create_all_page_file(page_divs):
-    with open("templates/all_template.html", "r", encoding="utf-8") as f:
-        template = f.read()
-        template = template.replace("{page_divs}", page_divs)
-        print(template[300:500])
-    with open(RESULT_DIR + "all/index.html", "w", encoding="utf-8") as result_f:
-        result_f.write(template)
-
-
-# DATA_WITH_RUBY_FILE_PATH のデータを元に、TEMPLATE_FILE_PATH のテンプレートを使って、RESULT_DIR にページを生成する。
-# 生成されるページは、data_with_ruby.csv の各行のデータを使って、テンプレートの中の {フィールド名} に対応する部分を置換する。
-# 生成されるページは、RESULT_DIR の中に、data_with_ruby.csv の各行のデータの "名前" フィールドの値をディレクトリ名に置き換え、その中に "index.html" として保存される。
-def main():
-    with open(DATA_WITH_RUBY_FILE_PATH, "r", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        name_without_ruby_list = []
-        pages_divs = ""
-        for row in reader:
-            name = row["名前"]
-            name_without_ruby = remove_ruby_tags(name)
-            dir_path = os.path.join(RESULT_DIR, name_without_ruby)
-            os.makedirs(dir_path, exist_ok=True)
-            
-            with open(TEMPLATE_FILE_PATH, "r", encoding="utf-8") as template_f:
-                template = template_f.read()
-                for field in fields:
-                    # field = field.replace("https://highsto.net/", "../../")
-                    template = template.replace("{" + field + "}", row[field].replace("https://highsto.net/", "../../"))
-                    template = template.replace("{" + field + "ルビなし}", remove_ruby_tags(row[field].replace("https://highsto.net/", "../../")))
-                with open(os.path.join(dir_path, "index.html"), "w", encoding="utf-8") as result_f:
-                    result_f.write(template)
-                    
-            with open(PAGES_DIV_TEMPLATE_FILE_PATH, "r", encoding="utf-8") as pages_div_template_f:
-                pages_div_template = pages_div_template_f.read()
-                for field in fields:
-                    # field = field.replace("https://highsto.net/", "../../")
-                    pages_div_template = pages_div_template.replace("{" + field + "}", row[field].replace("https://highsto.net/", "../../"))
-                    pages_div_template = pages_div_template.replace("{" + field + "ルビなし}", remove_ruby_tags(row[field].replace("https://highsto.net/", "../../")))
-                pages_divs += pages_div_template + "\n"
+    def create_index_page(names_without_ruby):
+        # pages/index.html を生成（各ページへのリンクをひたすら並べる）
+        with open(INDEX_TEMPLATE_FILE_PATH, "r", encoding="utf-8") as template_f:
+            template = template_f.read()
+            links = ""
+            for name in names_without_ruby:
+                links += f'<li><a href="{name}/index.html">{name}</a></li>\n'
+            template = template.replace("{links}", links)
+            with open(os.path.join(RESULT_DIR, "index.html"), "w", encoding="utf-8") as result_f:
+                result_f.write(template)
                 
-            name_without_ruby_list.append(name_without_ruby)
+    def create_all_page_file(page_divs):
+        with open(ALL_TEMPLATE_FILE_PATH, "r", encoding="utf-8") as f:
+            template = f.read()
+            template = template.replace("{page_divs}", page_divs)
+            print(template[300:500])
+        with open(os.path.join(RESULT_DIR, "all/index.html"), "w", encoding="utf-8") as result_f:
+            result_f.write(template)
 
-        print(name_without_ruby_list)
-        create_index_page(name_without_ruby_list)
-        create_all_page_file(pages_divs)
-        
-if __name__ == "__main__":
+
+    # DATA_WITH_RUBY_FILE_PATH のデータを元に、TEMPLATE_FILE_PATH のテンプレートを使って、RESULT_DIR にページを生成する。
+    # 生成されるページは、data_with_ruby.csv の各行のデータを使って、テンプレートの中の {フィールド名} に対応する部分を置換する。
+    # 生成されるページは、RESULT_DIR の中に、data_with_ruby.csv の各行のデータの "名前" フィールドの値をディレクトリ名に置き換え、その中に "index.html" として保存される。
+    def main():
+        with open(DATA_WITH_RUBY_FILE_PATH, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            name_without_ruby_list = []
+            pages_divs = ""
+            for row in reader:
+                name = row["名前"]
+                name_without_ruby = remove_ruby_tags(name)
+                dir_path = os.path.join(RESULT_DIR, name_without_ruby)
+                os.makedirs(dir_path, exist_ok=True)
+                os.makedirs(os.path.join(RESULT_DIR, "all"), exist_ok=True)
+                
+                with open(TEMPLATE_FILE_PATH, "r", encoding="utf-8") as template_f:
+                    template = template_f.read()
+                    for field in FIELDS:
+                        # field = field.replace("https://highsto.net/", "../../")
+                        template = template.replace("{" + field + "}", row[field].replace("https://highsto.net/", "../../"))
+                        template = template.replace("{" + field + "ルビなし}", remove_ruby_tags(row[field].replace("https://highsto.net/", "../../")))
+                    with open(os.path.join(dir_path, "index.html"), "w", encoding="utf-8") as result_f:
+                        result_f.write(template)
+                        
+                with open(PAGES_DIV_TEMPLATE_FILE_PATH, "r", encoding="utf-8") as pages_div_template_f:
+                    pages_div_template = pages_div_template_f.read()
+                    for field in FIELDS:
+                        # field = field.replace("https://highsto.net/", "../../")
+                        pages_div_template = pages_div_template.replace("{" + field + "}", row[field].replace("https://highsto.net/", "../../"))
+                        pages_div_template = pages_div_template.replace("{" + field + "ルビなし}", remove_ruby_tags(row[field].replace("https://highsto.net/", "../../")))
+                    pages_divs += pages_div_template + "\n"
+                    
+                name_without_ruby_list.append(name_without_ruby)
+
+            print(name_without_ruby_list)
+            create_index_page(name_without_ruby_list)
+            create_all_page_file(pages_divs)
+    
+    print("テンプレートからページを生成します。")
     main()
+    
+if __name__ == "__main__":
+    FIELDS = [
+        "異名",
+        "名前",
+        "時代",
+        "身分",
+        "生没年",
+        "効果解説トップキャラ",
+        "効果名1",
+        "効果解説1",
+        "効果解説1キャラ",
+        "効果名2",
+        "効果解説2",
+        "効果解説2キャラ",
+        "コラム",
+        "どんな人？",
+        "どんな人？トップキャラ",
+        "どんな人？内容キャラ",
+        "豆知識",
+        # "豆知識キャラ",
+        "クイズ",
+        "イラスト説明1のタイトル",
+        "イラスト説明1",
+        "イラスト説明1キャラ",
+        "イラスト説明2のタイトル",
+        "イラスト説明2",
+        "イラスト説明2キャラ",
+        "カード画像"
+    ]
+
+    DATA_WITH_RUBY_FILE_PATH = "data_with_ruby.csv"
+    TEMPLATE_FILE_PATH = "fefw_templates/template.html"
+    INDEX_TEMPLATE_FILE_PATH = "fefw_templates/index_template.html"
+    PAGES_DIV_TEMPLATE_FILE_PATH = "fefw_templates/pages_div_template.html"
+    ALL_TEMPLATE_FILE_PATH = "fefw_templates/all_template.html"
+    RESULT_DIR = "fefw_pages/"
+
+    template_to_pages(
+        FIELDS, DATA_WITH_RUBY_FILE_PATH, TEMPLATE_FILE_PATH,
+        INDEX_TEMPLATE_FILE_PATH, PAGES_DIV_TEMPLATE_FILE_PATH,
+        ALL_TEMPLATE_FILE_PATH, RESULT_DIR
+    )
 
 
