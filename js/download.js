5c5d995b73c742fbc6b31a99ee968dcd54c4644c
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2024-09-09 00:31:17 +0900

    レーダーチャートを強調表示

--- js/download.js
+++ js/download.js
@@ -33,6 +33,7 @@ document.querySelector("#downloadButton").addEventListener("click", async (e) =>
     } else {
         pageElements = [...document.querySelectorAll(".pages")];
     }
+    // pageElements = [...document.querySelectorAll(".raderChart")];
     const elemNum = pageElements.length;
 
     // const loadErrorImageNames = loadErrorImages.map((path) => decodeURI(path.split("/").pop()));
